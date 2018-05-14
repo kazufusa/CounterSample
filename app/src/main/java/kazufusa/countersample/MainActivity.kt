c78a16e446479dd5c3f9d7ec04b7fc20fee15f55
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2018-05-14 15:30:41 +0900

    Add initial version
    This version has some problems.
    
    1. If you rotate your device, the `count` value will be reset.
    2. It is hard to test. We can't unit test Activity class easily.
    3. Putting everything in a class, will make me a dumb coder. I am a
    dummy, but I don't want my code to look like one 😝.

--- app/src/main/java/kazufusa/countersample/MainActivity.kt
+++ app/src/main/java/kazufusa/countersample/MainActivity.kt
@@ -2,11 +2,23 @@ package kazufusa.countersample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        my_container.setOnClickListener { incrementCount() }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        incrementCount()
+    }
+
+    private fun incrementCount() {
+        my_text.text = (++count).toString()
     }
 }
