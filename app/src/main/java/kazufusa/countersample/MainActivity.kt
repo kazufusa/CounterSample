e22bcf21927f6737d2a6feb04d74d18da1094ac4
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2018-05-17 13:46:36 +0900

    Add dagger2 sample code

--- app/src/main/java/kazufusa/countersample/MainActivity.kt
+++ app/src/main/java/kazufusa/countersample/MainActivity.kt
@@ -4,10 +4,17 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import dagger.Component
+import dagger.Module
+import dagger.Provides
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModelFactory: ViewModelFactory
+    @Inject lateinit var infoMessenger : InfoMessenger
 
     private val counterViewModel: CounterViewModel by lazy {
         viewModelFactory = Injection.provideViewModelFactory(this)
@@ -24,9 +31,35 @@ class MainActivity : AppCompatActivity() {
         counterViewModel.getCounter().observe(this, changeObserver)
         lifecycle.addObserver(counterViewModel)
         my_container.setOnClickListener { counterViewModel.increment() }
+
+        DaggerMagicBox.create().inject(this)
+        infoMessenger.show()
     }
 
     private fun incrementCount(count: Int) {
         my_text.text = (count).toString()
+    }
+}
+
+@Component(modules = [Bag::class])
+interface MagicBox {
+    fun inject(app: MainActivity)
+}
+
+class InfoMessenger @Inject constructor() {
+    @Inject lateinit var info: Info
+
+    fun show(){
+        println(info.text)
+    }
+}
+
+data class Info(val text: String)
+
+@Module
+class Bag {
+    @Provides
+    open fun providesInfo(): Info {
+        return Info("Love Dagger 2")
     }
 }
--- readme.md
+++ readme.md
@@ -9,3 +9,8 @@ from https://medium.com/@elye.project/android-architecture-components-for-dummie
 - https://medium.com/exploring-android/android-architecture-components-testing-your-viewmodel-livedata-70177af89c6e
 - https://brightinventions.pl/blog/testing-android-viewmodels/
 - https://github.com/Helabs/mvvm-android-example
+
+# Dagger2
+
+- https://medium.com/@elye.project/dagger-2-for-dummies-in-kotlin-with-one-page-simple-code-project-618a5f9f2fe8
+- https://medium.com/@elye.project/dagger-2-for-dummies-in-kotlin-provides-and-module-b84dca1b0d03
